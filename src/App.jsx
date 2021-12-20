cb69f1757886182be684d6c365a48b50ecc766c9
Somchai <somchai.b@cvc.ac.th>
2021-12-20 10:05:06 +0700

    addName

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,25 @@ import SMath from 'string-math';
 
 export default function App() {
   const [Value, setValue] = useState("0")
+  const operator = ["+", "-", "*", "/"];
 
   const hanldeClick = (e) => {
+    const button = e.target.value
+    const lastchar = Value.charAt(Value.length - 1);
+    console.log(button)
     try {
-      const button = e.target.value
-      console.log(button)
       switch (button) {
         case 'clear':
           setValue("0")
           break;
         case '=':
-          setValue(`${SMath(Value)}`)
+          if (!operator.includes(lastchar)) setValue(`${SMath(Value)}`)
           break;
         case 'delete':
-          setValue(`${Value.slice(0, -1)}`)
+          if (Value.length > 1) setValue(`${Value.slice(0, -1)}`)
           break;
         default:
-          setValue(Value[0] === "0" || Value[0] === "E" ? button : Value.concat(button))
+          if (button !== lastchar) setValue(Value[0] === "0" || Value[0] === "E" ? button : Value.concat(button))
           break;
       }
     } catch (error) {
@@ -30,8 +32,13 @@ export default function App() {
   }
   return (
     <div>
-      <h1 style={{ fontSize: 50, color: "wheat", textAlign: "center" }}>เครื่องคิดเลข</h1>
+      <h1 style={{ fontSize: 50, color: "white", textAlign: "center" }}>เครื่องคิดเลข</h1>
       <Calulator buttonHandler={hanldeClick} data={Value} />
+      <div className='footer'>
+        <p>ณัชพล มะลิ เลขที่ 04</p>
+        <p>ศักดินนท์ คำมาสุข เลขที่ 14</p>
+        <p>สมชาย บริบูรณ์ เลขที่ 16</p>
+      </div>
     </div>
   )
 }
